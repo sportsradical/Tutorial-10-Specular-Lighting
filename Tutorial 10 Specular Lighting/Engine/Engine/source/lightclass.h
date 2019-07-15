4398a606b5631412cd2bf0c19c9f64072da24047
sportsradical <peereira@live.com.pt>
2019-07-15 15:10:34 +0100

    Add files via upload

---
+++ Tutorial 10 Specular Lighting/Engine/Engine/source/lightclass.h
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: lightclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _LIGHTCLASS_H_
+#define _LIGHTCLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+
+#include <DirectXMath.h>
+using namespace DirectX;
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: LightClass
+////////////////////////////////////////////////////////////////////////////////
+class LightClass
+{
+public:
+	LightClass();
+	LightClass(const LightClass& other);
+	~LightClass();
+
+	void SetSpecularColor(float x, float y, float z, float w);
+	void SetSpecularPower(float power);
+
+
+	void SetAmbientColor(float x, float y, float z, float w);
+	void SetDiffuseColor(float x, float y, float z, float w);
+	void SetDirection(float x, float y, float z);
+
+
+	XMFLOAT4 GetSpecularColor();
+	float GetSpecularPower();
+
+	XMFLOAT4 GetAmbientColor();
+	XMFLOAT4 GetDiffuseColor();
+	XMFLOAT3 GetDirection();
+
+private:
+	XMFLOAT4 m_specColor;
+	float m_sPecularColorPower;
+	XMFLOAT4 m_anbienteColor;
+	XMFLOAT4 m_diffuseColor;
+	XMFLOAT3 m_direction;
+	
+};
+
+#endif
